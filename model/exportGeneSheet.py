50a0398797e9b112a1545ff5ef21dd528293d340
zanert2 <zanert2@illinois.edu>
2026-05-14 10:01:14 -0500

    Changed name of input variable

--- model/exportGeneSheet.py
+++ model/exportGeneSheet.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-g', '--geneID', required=True, type=str, help="Number of parallel simulations")
 parser.add_argument('-od', '--outputdir', default='./', help="Output directory for simulation files")
 parser.add_argument('--coTrsc', default=True, action=argparse.BooleanOptionalAction)
-parser.add_argument('-dd', '--dataDir', type=str)
+parser.add_argument('-gd', '--genomeDir', type=str)
 parser.add_argument('-exp', '--export', default='global')
 parser.add_argument('-pb', '--proteinBinders', default=0, type=int)
 
@@ -31,7 +31,7 @@ genes = {}
 
 splice.getGeneData(args.dataDir, geneID, genes)
 
-splice.getIsoforms(sim, genes, args.dataDir, geneID)
+splice.getIsoforms(sim, genes, args.genomeDir, geneID)
 
 if args.export == 'global':
     splice.exportGlobalParameters(genes, simVars, outputDir=args.outputdir)
